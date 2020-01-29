7c1168a4ceaa2ae70a9de8196293456257e11d43
AasheeshGarg <aasheeshgarg28@gmail.com>
2020-01-28 22:45:58 -0800

    Update ProductRepository.java

--- src/main/java/com/example/AuctionManagementSystem/repository/ProductRepository.java
+++ src/main/java/com/example/AuctionManagementSystem/repository/ProductRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.AuctionManagementSystem.model.Product;
+import com.example.AuctionManagementSystem.model.User;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-	List<Product> findByUser(Long userId);
+	List<Product> findByUser(User user);
 
 }
